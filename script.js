1206bd4772ac76f7520704adbea546985c158dc7
Mateusz Lacek <mlacek19@comcast.net>
2023-02-06 10:04:43 -0600

    Add button for Resize. Insert box create loop into its own function ("draw"). Add event listener to button to prompt user for length of side of new grid.

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const container = document.querySelector(".container");
-for (let i = 0; i < 256; i++) {
-    const box = document.createElement('div');
-    box.classList.add('grid');
-    container.appendChild(box);
-    box.addEventListener('mouseover', () =>
-        box.setAttribute('style', 'background: blue'))
+const container = document.querySelector('.container');
+const resize = document.querySelector('button');
+let side = 16;
+draw(side);
+resize.addEventListener('click', () => {
+    side = prompt("How many squares per side would you like? (Limit 100)");
+    if (side > 100) {
+        side = prompt("Max length is 100. Please enter a number less than or equal to 100.")
+    }
+    draw(side);
+})
+
+function draw(side) {
+    const totalSize = side * side;
+
+    for (let i = 0; i < totalSize; i++) {
+        const box = document.createElement('div');
+        box.classList.add('grid');
+        container.appendChild(box);
+        box.addEventListener('mouseover', () =>
+            box.setAttribute('style', 'background: blue'))
+    }
 }
